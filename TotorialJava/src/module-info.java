286fd6c5f44435e6cd6a7a6d93b4dbf18cc63245
TomeuSabater <bmesabater@gmail.com>
2024-01-09 12:04:00 +0100

    Creacion Proyecto

---
+++ TotorialJava/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TotorialJava {
+}
---
+++ TotorialJava/src/tutgit/PrimeraClase.java
@@ -0,0 +1,11 @@
+package tutgit;
+
+public class PrimeraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//aqui va el código 
+
+	}
+
+}
