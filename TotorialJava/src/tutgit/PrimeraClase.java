0025265abe8ce4cc5a0b987cd2b444918005ee4a
TomeuSabater <bmesabater@gmail.com>
2024-01-09 12:11:55 +0100

    Mi segundo commit, añado una variable y mensaje.

--- TotorialJava/src/tutgit/PrimeraClase.java
+++ TotorialJava/src/tutgit/PrimeraClase.java
@@ -5,6 +5,11 @@ public class PrimeraClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//aqui va el código 
+		
+		System.out.println("Hola mundo");
+		
+		String nombre = "Tomeu";
+		System.out.println("Hola" + nombre);
 
 	}
 
